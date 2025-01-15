02314b810675284d330c97e69f77ddf34946fe79
Samuel Street <planetaryeclipse@gmail.com>
2025-01-15 13:33:07 -0800

    Disable right side driver

--- wombat/ros2_ws/src/wombat_drivers/wombat_drivers/open_loop_motor_driver.py
+++ wombat/ros2_ws/src/wombat_drivers/wombat_drivers/open_loop_motor_driver.py
@@ -7,14 +7,16 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 
 LEFT_WHEEL_SPEED_TOPIC = "left_wheel_speed_rpm"
-RIGHT_WHEEL_SPEED_TOPIC = "right_wheel_speed_rpm"
+# RIGHT_WHEEL_SPEED_TOPIC = "right_wheel_speed_rpm"
+
+# NOTE: due to lack of PWM signals from the RPi only the left motor is currently used
 
 # pwm signals for speed control
-LEFT_WHEEL_FWD_PWM_PIN = 10
-LEFT_WHEEL_REV_PWM_PIN = 11
+LEFT_WHEEL_FWD_PWM_PIN = 12 # pwm0
+LEFT_WHEEL_REV_PWM_PIN = 13 # pwm1
 
-RIGHT_WHEEL_FWD_PWM_PIN = 12
-RIGHT_WHEEL_FWD_PWM_PIN = 13
+# RIGHT_WHEEL_FWD_PWM_PIN = 45
+# RIGHT_WHEEL_FWD_PWM_PIN = 13
 
 WHEEL_PWM_FREQ = 20
 
@@ -24,11 +26,11 @@ WHEEL_PWM_MAX_DUTY = 80  # maximum percentage
 
 
 # signals for selection of direction input
-LEFT_WHEEL_FWD_ENABLE_PIN = 15
-LEFT_WHEEL_REV_ENABLE_PIN = 16
+LEFT_WHEEL_FWD_ENABLE_PIN = 23
+LEFT_WHEEL_REV_ENABLE_PIN = 24
 
-RIGHT_WHEEL_FWD_ENABLE_PIN = 17
-RIGHT_WHEEL_REV_ENABLE_PIN = 18
+# RIGHT_WHEEL_FWD_ENABLE_PIN = 17
+# RIGHT_WHEEL_REV_ENABLE_PIN = 18
 
 
 class OpenLoopMotorDriver(Node):
@@ -40,25 +42,25 @@ class OpenLoopMotorDriver(Node):
         # directional control for each wheel
         GPIO.setup(LEFT_WHEEL_FWD_ENABLE_PIN, GPIO.OUT)
         GPIO.setup(LEFT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
-        GPIO.setup(RIGHT_WHEEL_FWD_ENABLE_PIN, GPIO.OUT)
-        GPIO.setup(RIGHT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
+        # GPIO.setup(RIGHT_WHEEL_FWD_ENABLE_PIN, GPIO.OUT)
+        # GPIO.setup(RIGHT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
 
         # ensures that all speeds are disabled at startup
         GPIO.output(LEFT_WHEEL_FWD_ENABLE_PIN, GPIO.LOW)
         GPIO.output(LEFT_WHEEL_REV_ENABLE_PIN, GPIO.LOW)
-        GPIO.output(RIGHT_WHEEL_FWD_ENABLE_PIN, GPIO.LOW)
-        GPIO.output(RIGHT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
+        # GPIO.output(RIGHT_WHEEL_FWD_ENABLE_PIN, GPIO.LOW)
+        # GPIO.output(RIGHT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
 
         # speed control for each wheel
         self.left_fwd_pwm = GPIO.PWM(LEFT_WHEEL_FWD_PWM_PIN, WHEEL_PWM_FREQ)
         self.left_rev_pwm = GPIO.PWM(LEFT_WHEEL_REV_PWM_PIN, WHEEL_PWM_FREQ)
-        self.right_fwd_pwm = GPIO.PWM(RIGHT_WHEEL_FWD_PWM_PIN, WHEEL_PWM_FREQ)
-        self.right_rev_pwm = GPIO.PWM(RIGHT_WHEEL_REV_ENABLE_PIN, WHEEL_PWM_FREQ)
+        # self.right_fwd_pwm = GPIO.PWM(RIGHT_WHEEL_FWD_PWM_PIN, WHEEL_PWM_FREQ)
+        # self.right_rev_pwm = GPIO.PWM(RIGHT_WHEEL_REV_ENABLE_PIN, WHEEL_PWM_FREQ)
 
         self.left_fwd_pwm.start(0)
         self.left_rev_pwm.start(0)
-        self.right_fwd_pwm.start(0)
-        self.right_rev_pwm.start(0)
+        # self.right_fwd_pwm.start(0)
+        # self.right_rev_pwm.start(0)
 
     def __init__(self):
         super().__init__("open_loop_motor_driver_node")
@@ -68,12 +70,12 @@ class OpenLoopMotorDriver(Node):
         self.left_wheel_speed_subscription = self.create_subscription(
             Float32, LEFT_WHEEL_SPEED_TOPIC, self.left_wheel_speed_callback, 10
         )
-        self.right_wheel_speed_subscription = self.create_subscription(
-            Float32, RIGHT_WHEEL_SPEED_TOPIC, self.right_wheel_speed_callback, 10
-        )
+        # self.right_wheel_speed_subscription = self.create_subscription(
+        #     Float32, RIGHT_WHEEL_SPEED_TOPIC, self.right_wheel_speed_callback, 10
+        # )
 
         self.reverse_left_param = self.declare_parameter("reverse_left", False)
-        self.reverse_right_param = self.declare_parameter("reverse_right", False)
+        # self.reverse_right_param = self.declare_parameter("reverse_right", False)
 
     def set_wheel_speed_signals(
         self, speed: float, reverse: bool, fwd_pwm: GPIO.PWM, rev_pwm: GPIO.PWM
@@ -83,10 +85,10 @@ class OpenLoopMotorDriver(Node):
 
         if not reverse:
             fwd_pwm.ChangeDutyCycle(clamped_pwm_duty)
-            rev_pwm.ChangeDutyCycle(0)
+            # rev_pwm.ChangeDutyCycle(0)
         else:
             rev_pwm.ChangeDutyCycle(clamped_pwm_duty)
-            fwd_pwm.ChangeDutyCycle(0)
+            # fwd_pwm.ChangeDutyCycle(0)
 
     def left_wheel_speed_callback(self, msg: Float32):
         self.get_logger().info(f"Setting left wheel speed: {msg.data}")
@@ -99,12 +101,12 @@ class OpenLoopMotorDriver(Node):
 
     def right_wheel_speed_callback(self, msg: Float32):
         self.get_logger().info(f"Setting right wheel speed: {msg.data}")
-        self.set_wheel_speed_signals(
-            msg.data,
-            self.reverse_right_param.get_parameter_value(),
-            self.right_fwd_pwm,
-            self.right_rev_pwm,
-        )
+        # self.set_wheel_speed_signals(
+        #     msg.data,
+        #     self.reverse_right_param.get_parameter_value(),
+        #     self.right_fwd_pwm,
+        #     self.right_rev_pwm,
+        # )
 
 
 def main(args=None):
