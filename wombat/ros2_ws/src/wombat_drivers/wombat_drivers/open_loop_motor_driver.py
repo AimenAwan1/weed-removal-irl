4fe195fd15d182c397a5068c2771cafceb549668
Samuel Street <planetaryeclipse@gmail.com>
2025-01-17 17:30:47 -0500

    Remove GPIO enable pins to match testing behavior

--- wombat/ros2_ws/src/wombat_drivers/wombat_drivers/open_loop_motor_driver.py
+++ wombat/ros2_ws/src/wombat_drivers/wombat_drivers/open_loop_motor_driver.py
@@ -12,14 +12,10 @@ from std_msgs.msg import Float32
 # left/right wheel driver
 
 LEFT_WHEEL_SPEED_TOPIC = "left_wheel_speed_rpm"
-LEFT_WHEEL_FWD_ENABLE_PIN = 12
-LEFT_WHEEL_REV_ENABLE_PIN = 13
 LEFT_FWD_PWM_PCA_IDX = 0
 LEFT_REV_PWM_PCA_IDX = 1
 
 RIGHT_WHEEL_SPEED_TOPIC = "right_wheel_speed_rpm"
-RIGHT_WHEEL_FWD_ENABLE_PIN = 16
-RIGHT_WHEEL_REV_ENABLE_PIN = 17
 RIGHT_FWD_PWM_PCA_IDX = 2
 RIGHT_REV_PWM_PCA_IDX = 3
 
@@ -31,17 +27,6 @@ WHEEL_SPEED_DEADZONE_RADPS = 0.1
 
 
 class OpenLoopMotorDriver(Node):
-
-    def setup_gpio(self):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-
-        GPIO.setup(LEFT_WHEEL_FWD_ENABLE_PIN, GPIO.OUT)
-        GPIO.setup(LEFT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
-
-        GPIO.setup(RIGHT_WHEEL_FWD_ENABLE_PIN, GPIO.OUT)
-        GPIO.setup(RIGHT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
-
     def setup_pca9685(self):
         i2c = board.I2C()
         self.pca = PCA9685(i2c)
@@ -70,23 +55,17 @@ class OpenLoopMotorDriver(Node):
             Float32, RIGHT_WHEEL_SPEED_TOPIC, self.right_wheel_speed_callback, 10
         )
 
-        self.setup_gpio()
         self.setup_pca9685()
 
     def set_wheel_speed_signals(
         self,
         speed_rpm: float,
         reverse: bool,
-        fwd_enable_pin: int,
-        rev_enable_pin: int,
         fwd_pwm_idx: int,
         rev_pwm_idx: int,
     ):
         if abs(speed_rpm) <= WHEEL_SPEED_DEADZONE_RADPS:
             # disables both PWM inputs when in the desert
-            GPIO.output(fwd_enable_pin, GPIO.LOW)
-            GPIO.output(rev_enable_pin, GPIO.LOW)
-
             self.pca.channels[fwd_pwm_idx] = 0x0000
             self.pca.channels[rev_pwm_idx] = 0x0000
         else:
@@ -95,18 +74,12 @@ class OpenLoopMotorDriver(Node):
 
             if speed_rpm > 0 and not reverse:
                 # forward wheel movement is requested
-                self.pca.channels[fwd_pwm_idx] = clamped_pwm_duty
                 self.pca.channels[rev_pwm_idx] = 0x0000
-
-                GPIO.output(fwd_enable_pin, GPIO.HIGH)
-                GPIO.output(rev_enable_pin, GPIO.LOW)
+                self.pca.channels[fwd_pwm_idx] = clamped_pwm_duty
             else:
                 # reverse wheel movement is requested
                 self.pca.channels[fwd_pwm_idx] = 0x0000
                 self.pca.channels[rev_pwm_idx] = clamped_pwm_duty
-
-                GPIO.output(fwd_enable_pin, GPIO.LOW)
-                GPIO.output(rev_enable_pin, GPIO.HIGH)
 
     def left_wheel_speed_callback(self, msg: Float32):
         self.get_logger().info(f"Setting left wheel speed: {msg.data}")
@@ -114,8 +87,6 @@ class OpenLoopMotorDriver(Node):
         self.set_wheel_speed_signals(
             msg.data,
             self.reverse_left_param.get_parameter_value(),
-            LEFT_WHEEL_FWD_ENABLE_PIN,
-            LEFT_WHEEL_REV_ENABLE_PIN,
             LEFT_FWD_PWM_PCA_IDX,
             LEFT_REV_PWM_PCA_IDX,
         )
@@ -126,8 +97,6 @@ class OpenLoopMotorDriver(Node):
         self.set_wheel_speed_signals(
             msg.data,
             self.reverse_right_param.get_parameter_value(),
-            RIGHT_WHEEL_FWD_ENABLE_PIN,
-            RIGHT_WHEEL_REV_ENABLE_PIN,
             RIGHT_FWD_PWM_PCA_IDX,
             RIGHT_REV_PWM_PCA_IDX,
         )
