9b530aafe42d3f46cf35a48ef2f51b5ace16220d
Samuel Street <planetaryeclipse@gmail.com>
2025-01-17 03:46:26 -0500

    Add open loop motor driver for wheels

--- wombat/ros2_ws/src/wombat_drivers/wombat_drivers/open_loop_motor_driver.py
+++ wombat/ros2_ws/src/wombat_drivers/wombat_drivers/open_loop_motor_driver.py
@@ -1,112 +1,133 @@
 import rclpy
 from rclpy.node import Node
 
-import RPi.GPIO as GPIO
+import board
+from adafruit_pca9685 import PCA9685
+
+from RPi import GPIO
 
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
+# left/right wheel driver
+
 LEFT_WHEEL_SPEED_TOPIC = "left_wheel_speed_rpm"
-# RIGHT_WHEEL_SPEED_TOPIC = "right_wheel_speed_rpm"
+LEFT_WHEEL_FWD_ENABLE_PIN = 12
+LEFT_WHEEL_REV_ENABLE_PIN = 13
+LEFT_FWD_PWM_PCA_IDX = 0
+LEFT_REV_PWM_PCA_IDX = 1
 
-# NOTE: due to lack of PWM signals from the RPi only the left motor is currently used
+RIGHT_WHEEL_SPEED_TOPIC = "right_wheel_speed_rpm"
+RIGHT_WHEEL_FWD_ENABLE_PIN = 16
+RIGHT_WHEEL_REV_ENABLE_PIN = 17
+RIGHT_FWD_PWM_PCA_IDX = 2
+RIGHT_REV_PWM_PCA_IDX = 3
 
-# pwm signals for speed control
-LEFT_WHEEL_FWD_PWM_PIN = 12 # pwm0
-LEFT_WHEEL_REV_PWM_PIN = 13 # pwm1
+DEFAULT_PCA_FREQ = 60  # Hz
 
-# RIGHT_WHEEL_FWD_PWM_PIN = 45
-# RIGHT_WHEEL_FWD_PWM_PIN = 13
-
-WHEEL_PWM_FREQ = 20
-
-# wheel speed parameters
-WHEEL_MAX_SPEED_RADPS = 1
-WHEEL_PWM_MAX_DUTY = 80  # maximum percentage
-
-
-# signals for selection of direction input
-LEFT_WHEEL_FWD_ENABLE_PIN = 23
-LEFT_WHEEL_REV_ENABLE_PIN = 24
-
-# RIGHT_WHEEL_FWD_ENABLE_PIN = 17
-# RIGHT_WHEEL_REV_ENABLE_PIN = 18
+WHEEL_SPEED_MAX_PWM_DUTY = 0xCCCC  # 80%
+WHEEL_SPEED_MAX_RADPS = 1.5
+WHEEL_SPEED_DEADZONE_RADPS = 0.1
 
 
 class OpenLoopMotorDriver(Node):
+
     def setup_gpio(self):
-        # GPIO initialization (if not already completed)
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
 
-        # directional control for each wheel
         GPIO.setup(LEFT_WHEEL_FWD_ENABLE_PIN, GPIO.OUT)
         GPIO.setup(LEFT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
-        # GPIO.setup(RIGHT_WHEEL_FWD_ENABLE_PIN, GPIO.OUT)
-        # GPIO.setup(RIGHT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
 
-        # ensures that all speeds are disabled at startup
-        GPIO.output(LEFT_WHEEL_FWD_ENABLE_PIN, GPIO.LOW)
-        GPIO.output(LEFT_WHEEL_REV_ENABLE_PIN, GPIO.LOW)
-        # GPIO.output(RIGHT_WHEEL_FWD_ENABLE_PIN, GPIO.LOW)
-        # GPIO.output(RIGHT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
+        GPIO.setup(RIGHT_WHEEL_FWD_ENABLE_PIN, GPIO.OUT)
+        GPIO.setup(RIGHT_WHEEL_REV_ENABLE_PIN, GPIO.OUT)
 
-        # speed control for each wheel
-        self.left_fwd_pwm = GPIO.PWM(LEFT_WHEEL_FWD_PWM_PIN, WHEEL_PWM_FREQ)
-        self.left_rev_pwm = GPIO.PWM(LEFT_WHEEL_REV_PWM_PIN, WHEEL_PWM_FREQ)
-        # self.right_fwd_pwm = GPIO.PWM(RIGHT_WHEEL_FWD_PWM_PIN, WHEEL_PWM_FREQ)
-        # self.right_rev_pwm = GPIO.PWM(RIGHT_WHEEL_REV_ENABLE_PIN, WHEEL_PWM_FREQ)
+    def setup_pca9685(self):
+        i2c = board.I2C()
+        self.pca = PCA9685(i2c)
 
-        self.left_fwd_pwm.start(0)
-        self.left_rev_pwm.start(0)
-        # self.right_fwd_pwm.start(0)
-        # self.right_rev_pwm.start(0)
+        self.pca.reset()
+        self.pca.frequency = self.get_parameter("pca_freq")
+
+        # ensures that all motor commands are 0 at startup
+        self.pca.channels[LEFT_FWD_PWM_PCA_IDX].duty_cycle = 0
+        self.pca.channels[LEFT_REV_PWM_PCA_IDX].duty_cycle = 0
+
+        self.pca.channels[RIGHT_FWD_PWM_PCA_IDX].duty_cycle = 0
+        self.pca.channels[RIGHT_REV_PWM_PCA_IDX].duty_cycle = 0
 
     def __init__(self):
         super().__init__("open_loop_motor_driver_node")
-        self.setup_gpio()
 
-        # callbacks for new speed selection
+        self.pca_freq_param = self.declare_parameter("pca_freq", DEFAULT_PCA_FREQ)
+        self.reverse_left_param = self.declare_parameter("reverse_left", False)
+        self.reverse_right_param = self.declare_parameter("reverse_right", False)
+
         self.left_wheel_speed_subscription = self.create_subscription(
             Float32, LEFT_WHEEL_SPEED_TOPIC, self.left_wheel_speed_callback, 10
         )
-        # self.right_wheel_speed_subscription = self.create_subscription(
-        #     Float32, RIGHT_WHEEL_SPEED_TOPIC, self.right_wheel_speed_callback, 10
-        # )
+        self.right_wheel_speed_subscription = self.create_subscription(
+            Float32, RIGHT_WHEEL_SPEED_TOPIC, self.right_wheel_speed_callback, 10
+        )
 
-        self.reverse_left_param = self.declare_parameter("reverse_left", False)
-        # self.reverse_right_param = self.declare_parameter("reverse_right", False)
+        self.setup_gpio()
+        self.setup_pca9685()
 
     def set_wheel_speed_signals(
-        self, speed: float, reverse: bool, fwd_pwm: GPIO.PWM, rev_pwm: GPIO.PWM
+        self,
+        speed_rpm: float,
+        reverse: bool,
+        fwd_enable_pin: int,
+        rev_enable_pin: int,
+        fwd_pwm_idx: int,
+        rev_pwm_idx: int,
     ):
-        pwm_duty = WHEEL_PWM_MAX_DUTY / WHEEL_MAX_SPEED_RADPS * speed
-        clamped_pwm_duty = min(pwm_duty, WHEEL_PWM_MAX_DUTY)
-
-        if not reverse:
-            fwd_pwm.ChangeDutyCycle(clamped_pwm_duty)
-            # rev_pwm.ChangeDutyCycle(0)
+        if abs(speed_rpm) <= WHEEL_SPEED_DEADZONE_RADPS:
+            # disables both PWM inputs when in the desert
+            GPIO.output(fwd_enable_pin, GPIO.LOW)
+            GPIO.output(rev_enable_pin, GPIO.LOW)
         else:
-            rev_pwm.ChangeDutyCycle(clamped_pwm_duty)
-            # fwd_pwm.ChangeDutyCycle(0)
+            pwm_duty = WHEEL_SPEED_MAX_PWM_DUTY / WHEEL_SPEED_MAX_RADPS * abs(speed_rpm)
+            clamped_pwm_duty = min(pwm_duty, WHEEL_SPEED_MAX_PWM_DUTY)
+
+            if speed_rpm > 0 and not reverse:
+                # forward wheel movement is requested
+                self.pca.channels[fwd_pwm_idx] = clamped_pwm_duty
+                self.pca.channels[rev_pwm_idx] = 0x0000
+
+                GPIO.output(fwd_enable_pin, GPIO.HIGH)
+                GPIO.output(rev_enable_pin, GPIO.LOW)
+            else:
+                # reverse wheel movement is requested
+                self.pca.channels[fwd_pwm_idx] = 0x0000
+                self.pca.channels[rev_pwm_idx] = clamped_pwm_duty
+
+                GPIO.output(fwd_enable_pin, GPIO.LOW)
+                GPIO.output(rev_enable_pin, GPIO.HIGH)
 
     def left_wheel_speed_callback(self, msg: Float32):
         self.get_logger().info(f"Setting left wheel speed: {msg.data}")
+
         self.set_wheel_speed_signals(
             msg.data,
             self.reverse_left_param.get_parameter_value(),
-            self.left_fwd_pwm,
-            self.left_rev_pwm,
+            LEFT_WHEEL_FWD_ENABLE_PIN,
+            LEFT_WHEEL_REV_ENABLE_PIN,
+            LEFT_FWD_PWM_PCA_IDX,
+            LEFT_REV_PWM_PCA_IDX,
         )
 
     def right_wheel_speed_callback(self, msg: Float32):
         self.get_logger().info(f"Setting right wheel speed: {msg.data}")
-        # self.set_wheel_speed_signals(
-        #     msg.data,
-        #     self.reverse_right_param.get_parameter_value(),
-        #     self.right_fwd_pwm,
-        #     self.right_rev_pwm,
-        # )
+
+        self.set_wheel_speed_signals(
+            msg.data,
+            self.reverse_right_param.get_parameter_value(),
+            RIGHT_WHEEL_FWD_ENABLE_PIN,
+            RIGHT_WHEEL_REV_ENABLE_PIN,
+            RIGHT_FWD_PWM_PCA_IDX,
+            RIGHT_REV_PWM_PCA_IDX,
+        )
 
 
 def main(args=None):
