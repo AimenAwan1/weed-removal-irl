6e21e09a281d06ea29668635a17a00bbb394d204
Samuel Street <planetaryeclipse@gmail.com>
2025-01-18 12:47:33 -0500

    Change differential drive to use single twist message as input

--- wombat/ros2_ws/src/wombat_nav/wombat_nav/differential_drive.py
+++ wombat/ros2_ws/src/wombat_nav/wombat_nav/differential_drive.py
@@ -2,8 +2,9 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
-CHASSIS_SPEED_TOPIC = "chassis_speed"
-CHASSIS_ANG_VEL_TOPIC = "chassis_ang_vel"
+from geometry_msgs.msg import Twist
+
+CHASSIS_VEL_TOPIC = "chassis_vel"
 
 LEFT_WHEEL_SPEED_TOPIC = "left_wheel_speed_radps"
 RIGHT_WHEEL_SPEED_TOPIC = "right_wheel_speed_radps"
@@ -15,11 +16,8 @@ class DifferentialDrive(Node):
     def __init__(self):
         super().__init__("differential_driver_node")
 
-        self.chassis_speed_subscription = self.create_subscription(
-            Float32, CHASSIS_SPEED_TOPIC, self.chassis_speed_callback, 10
-        )
-        self.chassis_ang_vel_subscription = self.create_subscription(
-            Float32, CHASSIS_ANG_VEL_TOPIC, self.chassis_ang_vel_callback, 10
+        self.chassis_vel = self.create_subscription(
+            Twist, CHASSIS_VEL_TOPIC, self.chassis_vel_callback, 10
         )
 
         self.left_wheel_speed_publisher = self.create_publisher(
@@ -29,29 +27,19 @@ class DifferentialDrive(Node):
             Float32, RIGHT_WHEEL_SPEED_TOPIC, 10
         )
 
-        self.chassis_speed = 0
-        self.chassis_ang_vel = 0
-
-    def chassis_speed_callback(self, msg: Float32):
-        self.get_logger().debug(f"Speed received: {msg.data}")
-        self.chassis_speed = msg.data
-        self.publish_wheel_speeds()
-
-    def chassis_ang_vel_callback(self, msg: Float32):
-        self.get_logger().debug(f"Angular velocity received: {msg.data}")
-        self.chassis_ang_vel = msg.data
-        self.publish_wheel_speeds()
-
-    def publish_wheel_speeds(self):
-        right_wheel_speed_radps = (
-            self.chassis_speed + CHASSIS_WIDTH_M * self.chassis_ang_vel / 2
+    def chassis_vel_callback(self, msg: Twist):
+        self.get_logger().debug(
+            self, f"Chassis speed command received: v={msg.linear.x}, w={msg.angular.z}"
         )
-        left_wheel_speed_radps = (
-            self.chassis_speed - CHASSIS_WIDTH_M * self.chassis_ang_vel / 2
-        )
+        chassis_speed = msg.linear.x
+        chassis_ang_vel = msg.angular.z
+
+        right_wheel_speed_radps = chassis_speed + CHASSIS_WIDTH_M * chassis_ang_vel / 2
+        left_wheel_speed_radps = chassis_speed - CHASSIS_WIDTH_M * chassis_ang_vel / 2
 
         self.left_wheel_speed_publisher.publish(Float32(data=left_wheel_speed_radps))
         self.right_wheel_speed_publisher.publish(Float32(data=right_wheel_speed_radps))
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -61,6 +49,7 @@ def main(args=None):
 
     differential_drive.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
