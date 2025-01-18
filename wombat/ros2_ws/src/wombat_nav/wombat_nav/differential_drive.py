bcd0f9836c5f86f90b48ce19efbf4e2c2a58aae4
Samuel Street <planetaryeclipse@gmail.com>
2025-01-17 19:20:08 -0500

    Add robot differential drive controller

---
+++ wombat/ros2_ws/src/wombat_nav/wombat_nav/differential_drive.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+
+CHASSIS_SPEED_TOPIC = "chassis_speed"
+CHASSIS_ANG_VEL_TOPIC = "chassis_ang_vel"
+
+LEFT_WHEEL_SPEED_TOPIC = "left_wheel_speed_radps"
+RIGHT_WHEEL_SPEED_TOPIC = "right_wheel_speed_radps"
+
+CHASSIS_WIDTH_M = 0.4508
+
+
+class DifferentialDrive(Node):
+    def __init__(self):
+        super.__init__("differential_driver_node")
+
+        self.chassis_speed_subscription = self.create_subscription(
+            Float32, CHASSIS_SPEED_TOPIC, self.chassis_speed_callback, 10
+        )
+        self.chassis_ang_vel_subscription = self.create_subscription(
+            Float32, CHASSIS_ANG_VEL_TOPIC, self.chassis_ang_vel_callback, 10
+        )
+
+        self.left_wheel_speed_publisher = self.create_publisher(
+            Float32, LEFT_WHEEL_SPEED_TOPIC, 10
+        )
+        self.right_wheel_speed_publisher = self.create_publisher(
+            Float32, RIGHT_WHEEL_SPEED_TOPIC, 10
+        )
+
+        self.chassis_speed = 0
+        self.chassis_ang_vel = 0
+
+    def chassis_speed_callback(self, msg: Float32):
+        self.get_logger().info(f"Speed received: {msg.data}")
+        self.chassis_speed = msg.data
+
+    def chassis_ang_vel_callback(self, msg: Float32):
+        self.get_logger().info(f"Angular velocity received: {msg.data}")
+        self.chassis_ang_vel = msg.data
+
+    def publish_wheel_speeds(self):
+        right_wheel_speed_radps = (
+            self.chassis_speed + CHASSIS_WIDTH_M * self.chassis_ang_vel / 2
+        )
+        left_wheel_speed_radps = (
+            self.chassis_speed - CHASSIS_WIDTH_M * self.chassis_ang_vel / 2
+        )
+
+        self.left_wheel_speed_publisher.publish(Float32(data=left_wheel_speed_radps))
+        self.right_wheel_speed_publisher.publish(Float32(data=right_wheel_speed_radps))
