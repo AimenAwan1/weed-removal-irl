36f7dfb3823f879c8567a2cb5993321b256c9ffc
Samuel Street <planetaryeclipse@gmail.com>
2025-01-26 04:22:12 -0500

    Change to retrieval of demo wheel speeds

--- wombat/i2c-command-test-stm32/App/Src/i2c_cmd_resp.c
+++ wombat/i2c-command-test-stm32/App/Src/i2c_cmd_resp.c
@@ -10,7 +10,7 @@ static float cmd_left_radps = 4.20;
 static float cmd_right_radps = 5.29;
 
 static uint8_t rxBuffer[SPEED_DATA_LEN];
-static uint8_t txBuffer[SPEED_DATA_LEN] = {1, 2, 3, 4, 5, 6, 7, 8};
+static uint8_t txBuffer[SPEED_DATA_LEN];
 
 static uint32_t txCount = 0;
 static uint32_t totalTxCount = 0;
@@ -44,11 +44,9 @@ extern void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t transferDirect
         if (active_cmd == CMD_READ_ENCODER)
         {
             // retrieves the most recent encoder data and transmits it
-            // float *txSpeed = (float *)txBuffer;
-            // txSpeed[0] = encoder_left_radps;
-            // txSpeed[1] = encoder_right_radps;
-            totalTxCount = SPEED_DATA_LEN;
-            // HAL_I2C_Slave_Transmit(hi2c, txBuffer, 3, 10);
+            float *txSpeed = (float *)txBuffer;
+            txSpeed[0] = encoder_left_radps;
+            txSpeed[1] = encoder_right_radps;
             HAL_I2C_Slave_Seq_Transmit_IT(hi2c, txBuffer, 8, I2C_LAST_FRAME);
         }
         else
@@ -89,74 +87,64 @@ void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
 
 void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c)
 {
-    active_cmd = CMD_NONE;
-
-    // if (active_cmd == CMD_READ_ENCODER)
-    // {
-    //     ++txCount; // just finished transmitting a byte
-    //     if (txCount >= SPEED_DATA_LEN)
-    //     {
-    //         active_cmd = CMD_NONE;
-    //     }
-    //     else
-    //     {
-    //         HAL_I2C_Slave_Sequential_Transmit_IT(hi2c, rxBuffer + txCount, 1, I2C_NEXT_FRAME);
-    //     }
-    // }
-    // else
-    // {
-    //     // protocol failure
-    //     Error_Handler();
-    // }
+    if (active_cmd == CMD_READ_ENCODER)
+    {
+        active_cmd = CMD_NONE;
+    }
+    else
+    {
+        // protocol failure
+        Error_Handler();
+    }
 }
 
-static bool error_none = false;
-static bool error_berr = false;
-static bool error_arlo = false;
-static bool error_af = false;
-static bool error_ovr = false;
-static bool error_dma = false;
-static bool error_timeout = false;
-static bool error_size = false;
-static bool error_dma_param = false;
-static bool error_wrong_start = false;
+// static bool error_none = false;
+// static bool error_berr = false;
+// static bool error_arlo = false;
+// static bool error_af = false;
+// static bool error_ovr = false;
+// static bool error_dma = false;
+// static bool error_timeout = false;
+// static bool error_size = false;
+// static bool error_dma_param = false;
+// static bool error_wrong_start = false;
 
 void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c)
 {
-    switch (HAL_I2C_GetError(hi2c))
-    {
-    case HAL_I2C_ERROR_BERR:
-        error_berr = true;
-        break;
-    case HAL_I2C_ERROR_ARLO:
-        error_arlo = true;
-        break;
-    case HAL_I2C_ERROR_AF:
-        error_af = true;
-        break;
-    case HAL_I2C_ERROR_OVR:
-        error_ovr = true;
-        break;
-    case HAL_I2C_ERROR_DMA:
-        error_dma = true;
-        break;
-    case HAL_I2C_ERROR_TIMEOUT:
-        error_timeout = true;
-        break;
-    case HAL_I2C_ERROR_SIZE:
-        error_size = true;
-        break;
-    case HAL_I2C_ERROR_DMA_PARAM:
-        error_dma_param = true;
-        break;
-    case HAL_I2C_WRONG_START:
-        error_wrong_start = true;
-        break;
-    case HAL_I2C_ERROR_NONE:
-    default:
-        error_none = true;
-        break;
-    }
+    // switch (HAL_I2C_GetError(hi2c))
+    // {
+    // case HAL_I2C_ERROR_BERR:
+    //     error_berr = true;
+    //     break;
+    // case HAL_I2C_ERROR_ARLO:
+    //     error_arlo = true;
+    //     break;
+    // case HAL_I2C_ERROR_AF:
+    //     error_af = true;
+    //     break;
+    // case HAL_I2C_ERROR_OVR:
+    //     error_ovr = true;
+    //     break;
+    // case HAL_I2C_ERROR_DMA:
+    //     error_dma = true;
+    //     break;
+    // case HAL_I2C_ERROR_TIMEOUT:
+    //     error_timeout = true;
+    //     break;
+    // case HAL_I2C_ERROR_SIZE:
+    //     error_size = true;
+    //     break;
+    // case HAL_I2C_ERROR_DMA_PARAM:
+    //     error_dma_param = true;
+    //     break;
+    // case HAL_I2C_WRONG_START:
+    //     error_wrong_start = true;
+    //     break;
+    // case HAL_I2C_ERROR_NONE:
+    // default:
+    //     error_none = true;
+    //     break;
+    // }
 
     active_cmd = CMD_NONE;
     HAL_I2C_EnableListen_IT(hi2c);
