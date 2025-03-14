ff550e5f2c91910ac8fefc0a51d267b4dd76a46a
Aimen <a9awan@uwaterloo.ca>
2025-03-13 22:02:32 -0400

    detection with depth camera

--- wombat/ros2_ws/src/wombat_nav/wombat_nav/vision_detection.py
+++ wombat/ros2_ws/src/wombat_nav/wombat_nav/vision_detection.py
@@ -6,48 +6,48 @@ from std_msgs.msg import Float64MultiArray
 import numpy as np
 import cv2 as cv
 import math
+import pyrealsense2 as rs
+from collections import deque
 
 # BRG color values
 #yellow
-UPPER_MATCH_COLOR = np.array([153, 255, 255])
-LOWER_MATCH_COLOR = np.array([30, 120, 0])
+UPPER_MATCH_COLOR = np.array([90, 255, 255])
+LOWER_MATCH_COLOR = np.array([10, 140, 140])
 #white
 #LOWER_MATCH_COLOR = np.array([150,150,150])
 #UPPER_MATCH_COLOR = np.array([250, 250, 250])
 
-KNOWN_WIDTH_CM = 11  
-FOCAL_LENGTH = 886.9545454545455
 
-def merge_rectangles(self, rects, threshold=30.0):
-    
+def merge_rectangles(rects, threshold=30.0):
     merged = []
+    rects = deque(rects)  # Use deque for efficient popping from the front
 
     while rects:
-        x, y, w, h = rects.pop(0)
-        merged_rect = (x, y, x+w, y+h)
-
+        x, y, w, h = rects.popleft()
+        merged_rect = (x, y, x + w, y + h)
         i = 0
+
         while i < len(rects):
             x2, y2, w2, h2 = rects[i]
-            other_rect = (x2, y2, x2+w2, y2+h2)
+            other_rect = (x2, y2, x2 + w2, y2 + h2)
 
             if (max(merged_rect[0], other_rect[0]) - min(merged_rect[2], other_rect[2]) <= threshold and
                 max(merged_rect[1], other_rect[1]) - min(merged_rect[3], other_rect[3]) <= threshold):
+
                 merged_rect = (
                     min(merged_rect[0], other_rect[0]),
                     min(merged_rect[1], other_rect[1]),
                     max(merged_rect[2], other_rect[2]),
                     max(merged_rect[3], other_rect[3])
                 )
-                rects.pop(i)
-        else:
-            i += 1
+                rects.remove(rects[i])  # More efficient than pop(i), avoids shifting elements
+            else:
+                i += 1  # Only increment if not merging
 
         merged.append(merged_rect)
+    
     return merged
 
-def calculate_distance(focal_length, real_width, width_in_pixels):
-    return (real_width * focal_length) / width_in_pixels
 
 class VisionNode(Node):
     def __init__(self):
@@ -55,43 +55,52 @@ class VisionNode(Node):
         self.publisher = self.create_publisher(Float64MultiArray, 'detected_objects', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
 
-        self.cap = cv.VideoCapture(0)
-        self.cap.set(cv.CAP_PROP_FPS, 20)
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        self.pipeline.start(config)
         
     def timer_callback(self):
-        ret, frame = self.cap.read()
-        if not ret:
+        frame = self.pipeline.wait_for_frames()
+        depth = frame.get_depth_frame()
+        colour = frame.get_color_frame()
+
+        if not depth or not colour:
             self.get_logger().error('Failed to capture frame')
             return
         
         self.get_logger().info('Reading camera frame')
-        mask = cv.inRange(frame, LOWER_MATCH_COLOR, UPPER_MATCH_COLOR)
+
+        colour_image = np.asanyarray(colour.get_data())
+        depth_image = np.asanyarray(depth.get_data()) * 0.001
+
+        mask = cv.inRange(colour_image, LOWER_MATCH_COLOR, UPPER_MATCH_COLOR)
         edges = cv.Canny(mask, 100, 200)
         contours, _ = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-        rect_img = frame.copy()
+        rect_img = colour_image.copy()
         rects = [cv.boundingRect(cnt) for cnt in contours]
-        merged_rects = rects #merge_rectangles(self, rects)
+        merged_rects = merge_rectangles(rects)
 
         detected_objects = []
-        frame_center =frame.shape[1] // 2
+        frame_center = colour_image.shape[1] // 2
     
         for (x1, y1, x2, y2) in merged_rects:
-            w = x2 - x1
-            h = y2 - y1
-            if w < 30 or h < 30:
-                continue
+            #if w < 30 or h < 30:
+            #    continue
 
-            aspect_ratio = h / w
-            theta = np.arctan(aspect_ratio)
-            corrected_width = w / np.cos(theta)
+            distance = depth_image[(y1 + y2)//2, (x1 + x2)//2]
+            angle = (((x1 + x2)// 2 - frame_center) / 640  * (86 * math.pi / 180))
+            
+            self.get_logger().info(f"distance: {distance}")
+            self.get_logger().info(f"angle: {angle}")
 
-            distance = calculate_distance(FOCAL_LENGTH, KNOWN_WIDTH_CM, corrected_width)
-            angle = ((x1 + w / 2 - frame_center) * (math.pi / 3))
-            detected_objects.extend((distance, angle))
+            detected_objects.append(float(distance))
+            detected_objects.append(float(angle))
 
             rect_img = cv.rectangle(rect_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv.putText(rect_img, f"{distance:.2f} cm", (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+            cv.putText(rect_img, f"{distance:.2f} m", (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
         msg = Float64MultiArray()
         msg.data = detected_objects
@@ -104,8 +113,7 @@ class VisionNode(Node):
         cv.waitKey(1)
 
     def destroy(self):
-        if self.cap:
-            self.cap.release()
+        self.pipeline.stop()
         cv.destroyAllWindows()
         super().destroy_node()
         
