Fix ensuring rotation before forward movement

wombat/ros2_ws/src/wombat_nav/wombat_nav/waypoint_action_server.py
@@ -14,7 +14,7 @@ from nav_msgs.msg import Odometry
 
 from wombat_msgs.action import WaypointAction
 
-from .error_angle import compute_err_angle 
+from .error_angle import compute_err_angle
 
 WAYPOINT_ACTION = "waypoint_action"
 WAYPOINT_ACTION_FEEDBACK_HZ = 2
@@ -25,9 +25,9 @@ ODOMETRY_TOPIC = "/robot_position"
 
 CHASSIS_VEL_TOPIC = "/chassis_vel"
 
-KP_LINEAR = 1.0 # 0.8
+KP_LINEAR = 1.0  # 0.8
 KV_LINEAR = 0.0  # 0.001
-KI_LINEAR = 0.0  # 0.5/2
+KI_LINEAR = 0.05  # 0.5/2
 
 KP_ANGULAR = 1.5
 KV_ANGULAR = 0.0  # 0.1/1
@@ -35,7 +35,8 @@ KI_ANGULAR = 0.0  # 0.5/2
 
 CONTROL_LOOP_TIMER_HZ = 30
 
-INITIAL_CONTROLLER_ALIGNMENT_RAD = np.pi/48 # np.pi/6  # 30 degrees in alignment
+INITIAL_CONTROLLER_ALIGNMENT_RAD = np.pi / \
+    48  # np.pi/6  # 30 degrees in alignment
 INITIAL_CONTROLLER_ALIGNMENT_TIME = 0.25
 
 # once this close turn off angle controller (prevents jumping)
@@ -132,7 +133,8 @@ class WaypointActionServer(Node):
                 self.error[1],
                 self.error[0])
 
-            error_angular = compute_err_angle(self.current_ang, current_error_angle)
+            error_angular = compute_err_angle(
+                self.current_ang, current_error_angle)
             # if np.abs(current_error_angle) > 2*np.pi/3:
             #     error_angular = (
             #         current_error_angle+2*np.pi) % (2*np.pi) - (self.current_ang+2*np.pi) % (2*np.pi)
@@ -177,7 +179,8 @@ class WaypointActionServer(Node):
 
             # prevents jumping when a bit of overshoot results in the error vector inverting
             # causing the robot to begin spinning around before shutting off as within allowances
-            w = w if error_linear > DISTANCE_TILL_ANGLE_SHUTOFF_M else 0.0
+            # NOTE: always allow rotation if within the initial alignment phase
+            w = w if not self.aligned_with_direction or error_linear > DISTANCE_TILL_ANGLE_SHUTOFF_M else 0.0
 
             self.get_logger().info("before chassis vel publish")
 
@@ -191,9 +194,10 @@ class WaypointActionServer(Node):
 
             self.get_logger().info("after chassis vel publish")
 
-            # has reached the correct location
-            if np.linalg.norm(self.error) < WAYPOINT_ERROR_DIST_THRESHOLD:
-                self.waypoint_nav_enabled = False
+            # only check has reached the correct location after the initial rotation has been made
+            if self.aligned_with_direction:
+                if np.linalg.norm(self.error) < WAYPOINT_ERROR_DIST_THRESHOLD:
+                    self.waypoint_nav_enabled = False
 
     def execute_callback(self, goal_handle):
         self.get_logger().info("Starting navigation to waypoint")

wombat/ros2_ws/src/wombat_nav/wombat_nav/weeds_spray_action_server.py
@@ -7,18 +7,19 @@ import rclpy.executors
 from rclpy.node import Node
 from rclpy.action import ActionServer, ActionClient, CancelResponse
 
-from wombat_msgs.action import WeedsSprayAction, WaypointAction
+from wombat_msgs.action import WeedsSprayAction, WaypointAction, TurnAction
 from geometry_msgs.msg import Point
 from nav_msgs.msg import Odometry
 
 
 WEEDS_SPRAY_ACTION = "weeds_spray_action"
-WEED_SEPARATION_DIST = 0.1
+WEED_SEPARATION_DIST = 0.10 # 0.1
 CHASSIS_WIDTH_M = 0.4508
 
 DISTANCE_EPSILON = 1e-3
 
 WAYPOINT_ACTION = "waypoint_action"  # action server to allow movement
+TURN_ACTION = "turn_action"  # action server to allow turning
 
 
 class WeedsSprayActionServer(Node):
@@ -42,6 +43,8 @@ class WeedsSprayActionServer(Node):
         self.waypoint_client = ActionClient(
             self, WaypointAction, WAYPOINT_ACTION, callback_group=self.callback_group
         )
+        self.turn_action_client = ActionClient(
+            self, TurnAction, TURN_ACTION, callback_group=self.callback_group)
 
         self.get_logger().info("Initialized the weeds spray action server...")
 
@@ -62,7 +65,8 @@ class WeedsSprayActionServer(Node):
 
         # obtain the various locations of the weeds
         weed_positions = goal_handle.request.weed_locations.data
-        weed_positions = np.array(weed_positions).reshape((len(weed_positions) // 2, 2))
+        weed_positions = np.array(weed_positions).reshape(
+            (len(weed_positions) // 2, 2))
 
         self.get_logger().info(f"Received weed positions:\n{weed_positions}")
 
@@ -76,7 +80,8 @@ class WeedsSprayActionServer(Node):
 
             # generate the augmented waypoint close to the weed location along
             # a path starting at the current location of the weed
-            current_pos = np.array([self.current_position.x, self.current_position.y])
+            current_pos = np.array(
+                [self.current_position.x, self.current_position.y])
             error_to_target = weed_pos - current_pos
             dist_to_target = np.linalg.norm(error_to_target)
 
@@ -87,15 +92,24 @@ class WeedsSprayActionServer(Node):
                 # can move to a location along the axis joining the current location and weed
                 error_to_target_dir = error_to_target / dist_to_target
 
-            dist_to_auxiliarly = dist_to_target - (CHASSIS_WIDTH_M / 2 + WEED_SEPARATION_DIST)
+            dist_to_auxiliarly = dist_to_target - \
+                (CHASSIS_WIDTH_M / 2 + WEED_SEPARATION_DIST)
             auxiliary_point = current_pos + dist_to_auxiliarly * error_to_target_dir
 
             # navigate to this point prior to activating the sprayer
-            self.get_logger().info(f"Navigating to auxilliary point: {auxiliary_point}")
+            self.get_logger().info(
+                f"Navigating to auxilliary point: {auxiliary_point}")
             waypoint_goal = WaypointAction.Goal()
-            waypoint_goal.target = Point(x=auxiliary_point[0], y=auxiliary_point[1])
+            waypoint_goal.target = Point(
+                x=auxiliary_point[0], y=auxiliary_point[1])
             self.waypoint_client.send_goal(waypoint_goal)
             self.get_logger().info("Reached auxiliary point")
+
+            # rotate to face the weed before activating the sprayer
+            rotate_goal = TurnAction.Goal()
+            rotate_goal.target.data = np.arctan2(
+                weed_pos[1] - self.current_position.y, weed_pos[0] - self.current_position.x)
+            self.turn_action_client.send_goal(rotate_goal)
 
             self.get_logger().info("ACTIVATING SPRAYER")
 